59e5c7a603a38ca2dce9d8002a12e34eaf402df3
DrMaruki97 <150847675+DrMaruki97@users.noreply.github.com>
2024-10-28 14:58:29 +0000

    CI: Modified test azure function

--- MyProjFolder/function_app.py
+++ MyProjFolder/function_app.py
@@ -9,18 +9,36 @@ app = func.FunctionApp()
 def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    surname =req.params.get('surname')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    ingredient_name = req.params.get('ingredient_name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name} {surname}. This HTTP triggered function executed successfully.")
+    if not ingredient_name:
+
+        return func.HttpResponse('Inserire il nome dell\'ingrediente da ricercare nel databse CIR')
+        
+
+
+    if ingredient_name:
+
+        link = ''
+
+        with open('cir_db.json', 'r') as file:
+            table = json.load(file)
+
+        for el in table['results'][0]:
+
+            if el['pcpc_ingredientname'] == ingredient_name or el['pcpc_ciringredientname'] == ingredient_name:
+
+                link = f'https://cir-reports.cir-safety.org/cir-ingredient-status-report/?id={el["pcpc_ingredientid"]}'
+                return func.HttpResponse(f'Pagina CIR dell\'ingrediente richiesto: {link}')
+            
+            else:
+                continue
+
+        if not link:
+
+            return func.HttpResponse(f'Impossibile trovare una pagine per l\'ingrediente {ingredient_name}')
+
+
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
