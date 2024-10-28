0dbfdc558dfb18f10b226ebf1488580982a86468
DrMaruki97 <150847675+DrMaruki97@users.noreply.github.com>
2024-10-28 15:51:22 +0000

    CI: new azure functions added

--- MyProjFolder/function_app.py
+++ MyProjFolder/function_app.py
@@ -5,8 +5,8 @@ import logging
 
 app = func.FunctionApp()
 
-@app.route(route="MyHttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
-def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="get_main", auth_level=func.AuthLevel.ANONYMOUS)
+def get_main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     ingredient_name = req.params.get('ingredient_name')
@@ -44,3 +44,58 @@ def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+    
+
+@app.route(route="get_pdf_link", auth_level=func.AuthLevel.ANONYMOUS)
+def get_pdf_link(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
+    url_base = 'https://cir-reports.cir-safety.org/'
+
+    web_page = req.get(link,headers=header)
+    page = BeautifulSoup(web_page.text,'html.parser')
+
+    # I link relativi alle fonti sono salvati in tag 'tr' che compongono una tabella, per cui estraiamo solo quelli
+
+    righe = page.find_all('tr')
+
+    # Il primo tr è sempre riservato all'intestazione della tabella, per cui se è presente un solo tr significa
+    # che non è presente alcuna fonte per l'ingrediente in esame
+
+    if len(righe)>1:
+
+        # Poichè alcune righe sono popolate da link deprecati, cicliamo su tutte le righe per trovare la prima
+        # che presenti un link valido, ovvero iniziante con '../'
+
+        for i in range(1,len(righe)):
+            riga = i      
+            report = righe[i].a['href']
+            if report[0] == '.':
+                break
+
+        # Controlliamo che un link valido sia stato trovato
+
+        if report[0] == '.':
+                
+                # Se in una riga è presente un link valido, componiamo il link definitivo alla fonte e ci salviamo
+                # anche la data in cui questa è stata rilasciata e il suo "nome proprio"
+
+                final_url = url_base + report[report.index('/')+1:]
+                date = righe[riga].find_all('td')[-1].text
+                pdf_name = righe[riga].find_all('td')[-2].text
+        else:
+            final_url = ''
+    else:
+        final_url = ''
+
+    return func.HttpResponse(final_url)
+
+
+@app.route(route="get_values", auth_level=func.AuthLevel.ANONYMOUS)
+def get_values(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+@app.route(route="load_values", auth_level=func.AuthLevel.ANONYMOUS)
+def load_values(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
