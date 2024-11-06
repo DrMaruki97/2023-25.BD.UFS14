94c241f56ba991b140c81fafe15a4542750bec46
DrMaruki97 <150847675+DrMaruki97@users.noreply.github.com>
2024-11-06 16:06:04 +0000

    feat: added extracting and loading az functions

--- MyProjFolder/function_app.py
+++ MyProjFolder/function_app.py
@@ -2,8 +2,14 @@ import azure.functions as func
 import datetime
 import json
 import logging
-import requests as req
+import regex as re
+import pypdf as pdf
+import requests as rq
+from cir_functions import get_source_table, get_pdf_values, sorting_func
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 from bs4 import BeautifulSoup
+from io import BytesIO
 
 app = func.FunctionApp()
 
@@ -34,7 +40,7 @@ def get_main(req: func.HttpRequest) -> func.HttpResponse:
                 payload = {'ingredient_name': ingredient_name,
                            'cir_name': el['pcpc_ciringredientname'],
                            'main_link': link}
-                return func.HttpResponse(json.dump(payload),mimetype= 'application/json')
+                return func.HttpResponse(payload,mimetype='application/json')
             
             else:
                 continue
@@ -51,62 +57,105 @@ def get_main(req: func.HttpRequest) -> func.HttpResponse:
         )
     
 
-@app.route(route="get_pdf_link", auth_level=func.AuthLevel.ANONYMOUS)
-def get_pdf_link(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="get_pdf_values", auth_level=func.AuthLevel.ANONYMOUS)
+def get_pdf_values(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
     url_base = 'https://cir-reports.cir-safety.org/'
 
-    web_page = req.get(req,headers=header)
-    page = BeautifulSoup(web_page.text,'html.parser')
+    main_url = req.params.get('req_url')
 
-    # I link relativi alle fonti sono salvati in tag 'tr' che compongono una tabella, per cui estraiamo solo quelli
-
-    righe = page.find_all('tr')
-
-    # Il primo tr è sempre riservato all'intestazione della tabella, per cui se è presente un solo tr significa
-    # che non è presente alcuna fonte per l'ingrediente in esame
-
-    if len(righe)>1:
-
-        # Poichè alcune righe sono popolate da link deprecati, cicliamo su tutte le righe per trovare la prima
-        # che presenti un link valido, ovvero iniziante con '../'
-
-        for i in range(1,len(righe)):
-            riga = i      
-            report = righe[i].a['href']
-            if report[0] == '.':
-                break
-
-        # Controlliamo che un link valido sia stato trovato
-
-        if report[0] == '.':
-                
-                # Se in una riga è presente un link valido, componiamo il link definitivo alla fonte e ci salviamo
-                # anche la data in cui questa è stata rilasciata e il suo "nome proprio"
-
-                final_url = url_base + report[report.index('/')+1:]
-                date = righe[riga].find_all('td')[-1].text
-                pdf_name = righe[riga].find_all('td')[-2].text
-        else:
-            final_url = ''
-            date = ''
-            pdf_name = ''
-    else:
-        final_url = ''
-        date = ''
-        pdf_name = ''
-
+    web_page = rq.get(main_url,headers=header)
     
+    righe = get_source_table(web_page)
 
-    return func.HttpResponse(final_url)
+    payload = get_pdf_values(righe)    
+
+    return func.HttpResponse(payload,mimetype='application/json')
 
 
 @app.route(route="get_values", auth_level=func.AuthLevel.ANONYMOUS)
 def get_values(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
+
+
+    pdf_link = req.params.get('pdf_link')
+    payload = {'valori_noael' : '',
+               'contesti_noael' : '',
+               'valori_ld50' : '',
+               'contesti_ld50' : ''}
+
+    try:
+
+        response = rq.get(pdf_link,headers=header)
+        file = BytesIO(response.content)            
+        pdf_text=pdf.PdfReader(file)
+
+        text = ''.join([x.extract_text() for x in pdf_text.pages])
+        text = text.replace('\n','').replace('\r','')
+
+
+        noael_pattern = r'(.{0,50}\bNOAEL\b.{0,100}(\b\d+\s*[\.,]*\d*\s*.g/kg\s*[\s*bw|body\s*weight]*\s*/d[a-zA-Z]{0,3}\b).{0,50})'
+        noael_values = re.finditer(noael_pattern,text)
+
+        temp_noaels = []
+        for el in noael_values:
+            temp_noaels.append((el.group(2),el.group(1)))
+
+        if temp_noaels:
+
+            temp_noaels.sort(key=sorting_func)
+
+            valori_noael = [x[0] for x in temp_noaels]
+            contesti_noael = [x[1] for x in temp_noaels]
+        else:
+            valori_noael = ''
+            contesti_noael = ''
+
+        ld50_pattern = r'(.{0,50}\bLD\s*50\b.{0,100}(\b\d+\s*[\.,]*\d*\s*.g/kg[\s*bw|body\s*weight]*\b).{0,50})'
+        ld50_values = re.finditer(ld50_pattern,text)
+        temp_ld50s = []
+        for el in ld50_values:
+            temp_ld50s.append((el.group(2),el.group(1)))
+        if temp_ld50s:
+            temp_ld50s.sort(key=sorting_func)
+            valori_ld50 = [x[0] for x in temp_ld50s]
+            contesti_ld50 = [x[1] for x in temp_ld50s]
+        else:
+            valori_ld50 = ''
+            contesti_ld50 = ''
+
+        payload['contesti_ld50'] = contesti_ld50
+        payload['contesti_noael'] = contesti_noael
+        payload['valori_ld50'] = valori_ld50
+        payload['valori_noael'] = valori_noael
+
+        return func.HttpResponse(payload,mimetype='application/json')
+
+    
+
+    except:
+        return func.HttpResponse(payload,mimetype='application/json')
+        
+
+
+    
 
 @app.route(route="load_values", auth_level=func.AuthLevel.ANONYMOUS)
 def load_values(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+
+    mongo_key = req.params.get('db_key')
+    values = req.params.get('value')
+
+    uri = f'mongodb+srv://lucagiovagnoli:{mongo_key}@ufs13.dsmvdrx.mongodb.net/'
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    db = client['INCI2']
+
+    inserimento = db.Ingredienti.insert_one(values)
+    if inserimento.acknowledged:
+        return func.HttpResponse(status_code=200)
+    else: 
+        return func.HttpResponse(status_code=404)
